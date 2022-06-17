6ecdb2ea56bb8df86e27b274e6ca1b615fc0fd63
silviawolves <silviacanepa.w@gmail.com>
2022-06-17 15:15:52 +0200

    bonus - completo

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ new Vue({
     methods: {
         randomMail() {
             this.arrayMail = []
+            this.cicloFinito = false
             for (let i = 0; i < 10; i++) {
 
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
